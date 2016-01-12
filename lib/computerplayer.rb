6252fd644301991c4521f475657d090043b15d70
Gilmoursa <gilmoursa@gmail.com>
2016-01-12 11:54:23 -0500

    computer player is smarter

--- lib/computerplayer.rb
+++ lib/computerplayer.rb
@@ -11,7 +11,10 @@ module TicTacToe
       puts "The computer is playing...".cyan
       sleep(0.5) #computer "thinks" for half a second, this seemed more realistic
       return if @game.game_over?
-      space = @game.available_moves.sample
+      space = (@game.available_moves & [1,3,5,7,9]).first
+      if space.nil? 
+        space = @game.available_moves.first
+      end
       @game.place_piece(space, @piece)
     end
   end
